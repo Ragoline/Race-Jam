347fcb6561cf01f636f069ee2ffa80b47550cab8
Anton Vodchits <anton.vodchits@yandex.ru>
2023-03-16 18:58:24 +0300

    Супер!
    Оч много работы с экранами в главном меню

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -5,9 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private GameObject _windows;
+    [SerializeField] private GameObject _storyWindow;
+    [SerializeField] private GameObject _racingWindow;
+    [SerializeField] private GameObject _dailyGiftWindow;
+    [SerializeField] private GameObject _dailyQuestWindow;
+    [SerializeField] private GameObject _optionsWindow;
+
+    private bool up = false;
+
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Escape))
+            CloseWindows();
+        if (up)
+        {
+            if (_storyWindow.activeSelf && _storyWindow.transform.position.y < Screen.height / 2f)
+                _storyWindow.transform.position = new Vector2(Screen.width / 2, _storyWindow.transform.position.y + 10f);
+            if (_racingWindow.activeSelf && _racingWindow.transform.position.y < Screen.height / 2f)
+                _racingWindow.transform.position = new Vector2(Screen.width / 2, _racingWindow.transform.position.y + 10f);
+            if (_dailyGiftWindow.activeSelf && _dailyGiftWindow.transform.position.y < Screen.height / 2f)
+                _dailyGiftWindow.transform.position = new Vector2(Screen.width / 2, _dailyGiftWindow.transform.position.y + 10f);
+            if (_dailyQuestWindow.activeSelf && _dailyQuestWindow.transform.position.y < Screen.height / 2f)
+                _dailyQuestWindow.transform.position = new Vector2(Screen.width / 2, _dailyQuestWindow.transform.position.y + 10f);
+            if (_optionsWindow.activeSelf && _optionsWindow.transform.position.y < Screen.height / 2f)
+                _optionsWindow.transform.position = new Vector2(Screen.width / 2, _optionsWindow.transform.position.y + 10f);
+        }
+        else
+        {
+            HideWindow(_storyWindow);
+            HideWindow(_racingWindow);
+            HideWindow(_dailyGiftWindow);
+            HideWindow(_dailyQuestWindow);
+            HideWindow(_optionsWindow);
+        }
     }
 
     public void ButtonClick(int num)
@@ -42,22 +73,62 @@ public class MenuManager : MonoBehaviour
 
     private void OpenWindow(int num)
     {
+        up = true;
+        _windows.SetActive(true);
+        _storyWindow.SetActive(false);
+        _racingWindow.SetActive(false);
+        _dailyGiftWindow.SetActive(false);
+        _dailyQuestWindow.SetActive(false);
+        _optionsWindow.SetActive(false);
         switch (num)
         {
             case 0: // story window
+                _storyWindow.transform.position = new Vector2(Screen.width / 2, Screen.height * (up ? -1 : 1));
+                _storyWindow.SetActive(true);
                 break;
 
             case 1: // racing window
+                _racingWindow.transform.position = new Vector2(Screen.width / 2, Screen.height * (up ? -1 : 1));
+                _racingWindow.SetActive(true);
                 break;
 
             case 2: // options window
+                _optionsWindow.transform.position = new Vector2(Screen.width / 2, Screen.height * (up ? -1 : 1));
+                _optionsWindow.SetActive(true);
                 break;
 
             case 3: // daily gift window
+                _dailyGiftWindow.transform.position = new Vector2(Screen.width / 2, Screen.height * (up ? -1 : 1));
+                _dailyGiftWindow.SetActive(true);
                 break;
 
             case 5: // daily quest window
+                _dailyQuestWindow.transform.position = new Vector2(Screen.width / 2, Screen.height * (up ? -1 : 1));
+                _dailyQuestWindow.SetActive(true);
                 break;
+        }
+    }
+
+    public void CloseWindows()
+    {
+        up = false;
+    }
+
+    private void HideWindow(GameObject go)
+    {
+        if (go.activeSelf)
+        {
+            if (go.transform.position.y < Screen.height * 2f)
+                go.transform.position = new Vector2(Screen.width / 2, go.transform.position.y + 20f);
+            else
+            {
+                _windows.SetActive(false);
+                _storyWindow.SetActive(false);
+                _racingWindow.SetActive(false);
+                _dailyGiftWindow.SetActive(false);
+                _dailyQuestWindow.SetActive(false);
+                _optionsWindow.SetActive(false);
+            }
         }
     }
 
--- Assets/Scripts/Racing/Vehicle.cs
+++ Assets/Scripts/Racing/Vehicle.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-// todo сделать у автомобилей возможность поворачивать
+
 public class Vehicle : RoadObject
 {
     [SerializeField] private Image _image;
     public int Length { get; private set; }
-    private bool obstacle = true;
-    private float speed;
+    private bool obstacle = true, moving = false, right;
+    private float speed, move = 180f;
 
     private void Start()
     {
@@ -26,10 +26,18 @@ public class Vehicle : RoadObject
                 Destroy(gameObject);
                 GameManager.Instance.Vehicle = -2;
             }
-            if (obstacle && ((Length == 1 && Mathf.Abs(transform.position.y - CarController.Instance.gameObject.transform.position.y) < 220 && Mathf.Abs(transform.position.x - CarController.Instance.gameObject.transform.position.x) < 138) || (Length == 2 && Mathf.Abs(transform.position.y - CarController.Instance.gameObject.transform.position.y) < 125 && Mathf.Abs(transform.position.x - CarController.Instance.gameObject.transform.position.x) < 275)))
+            if (obstacle && ((Length == 1 && Mathf.Abs(transform.position.y - CarController.Instance.gameObject.transform.position.y) < 213 && Mathf.Abs(transform.position.x - CarController.Instance.gameObject.transform.position.x) < 150) || (Length == 2 && Mathf.Abs(transform.position.y - CarController.Instance.gameObject.transform.position.y) < 125 && Mathf.Abs(transform.position.x - CarController.Instance.gameObject.transform.position.x) < 275)))
             {
                 CarController.Instance.Crash();
                 obstacle = false;
+            }
+            if (!moving && move > 0f && Random.Range(0, 10000) == 10)
+            {
+                Move();
+            }
+            if (moving)
+            {
+                Step();
             }
         }
     }
@@ -49,5 +57,42 @@ public class Vehicle : RoadObject
         var x = transform.position.x + Position * 180;
         transform.position = new Vector2(x, transform.position.y);
         gameObject.transform.SetParent(GameManager.Instance.Canvas.transform);
+    }
+
+    private void Move()
+    {
+        Debug.Log("Move");
+        moving = true;
+        switch (Position)
+        {
+            case -1:
+                right = true;
+                break;
+
+            case 0:
+                if (Random.Range(0, 2) == 0)
+                    right = true;
+                else
+                    right = false;
+                break;
+
+            case 1:
+                right = false;
+                break;
+
+        }
+        if (right)
+            Position++;
+        else
+            Position--;
+        GameManager.Instance.Vehicle = Position;
+    }
+
+    private void Step()
+    {
+        transform.position = new Vector2(transform.position.x + (right ? speed / 10f : -speed / 10f), transform.position.y);
+        move -= speed / 10f;
+        if (move <= 0f)
+            moving = false;
     }
 }
